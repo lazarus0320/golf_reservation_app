7cd6ba0f7d1b0741fe37415de7ad59d1e8c56c78
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-09-02 22:27:03 +0900

    fix: 스케줄 예약 페이지 전환 관련 버그 수정

--- lib/screens/reservation_page2.dart
+++ lib/screens/reservation_page2.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:golf_regist_app/controller/reservation_calendar_controller.dart';
-import 'package:golf_regist_app/widgets/PriorityListTile.dart';
-import 'package:golf_regist_app/widgets/reservation_btn.dart';
-import 'package:golf_regist_app/widgets/reservation_calendar_form2.dart';
-import 'package:golf_regist_app/widgets/reservation_timeset_form.dart';
-import 'package:intl/intl.dart';
-import 'package:table_calendar/table_calendar.dart';
-import 'package:intl/date_symbol_data_local.dart';
 
 import '../widgets/ReservationPersonnelAndTimesetForm.dart';
-
-
+import '../widgets/reservation_btn.dart';
+import '../widgets/reservation_calendar_form2.dart';
 
 class ReservationPage2 extends StatefulWidget {
   const ReservationPage2({Key? key}) : super(key: key);
@@ -20,158 +11,178 @@ class ReservationPage2 extends StatefulWidget {
   _ReservationPage2State createState() => _ReservationPage2State();
 }
 
-class _ReservationPage2State extends State<ReservationPage2> {
-  final PageController _pageController = PageController(initialPage: 0);
-  int currentPage = 0;
+class _ReservationPage2State extends State<ReservationPage2>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+  late Widget currentPage;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(_handleTabSelection);
+    currentPage = ReservationCalendarForm2(); // Initialize with the first form
+  }
+
+  void _handleTabSelection() {
+    setState(() {});
+  }
 
   @override
   void dispose() {
-    _pageController.dispose(); // PageController 해제
+    _tabController.dispose();
     super.dispose();
   }
 
-  void onPageChanged(int page) {
+  void switchPage(Widget newPage) {
     setState(() {
-      currentPage = page;
+      currentPage = newPage;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('골프 스케줄 예약/취소'),
-          centerTitle: true,
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () {},
-            ),
-          ],
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(100.0),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 20.0),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('골프 스케줄 예약/취소'),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {},
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(100.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Row(
+                  children: [
+                    const Text('골프장'),
+                    Container(
+                      width: 100.0,
+                      margin: const EdgeInsets.only(left: 10.0),
+                      color: Colors.grey[200],
+                      child: TextField(
+                        controller: TextEditingController(text: '드비치'),
+                        enabled: false,
+                        showCursor: false,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              TabBar(
+                controller: _tabController,
+                indicatorColor: Colors.black,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                tabs: const [
+                  Tab(text: '스케줄 예약'),
+                  Tab(text: '스케줄 조회/취소'),
+                  Tab(text: '예약 결과'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          // Content for the '스케줄 예약' tab (Page 1)
+          Column(
+            children: [
+              Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 30, top: 20),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: List<Widget>.generate(2, (index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                        width: 15.0,
+                        height: 15.0,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: currentPage is ReservationCalendarForm2
+                              ? (index == 0 ? Colors.black : Colors.white)
+                              : Colors.black,
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 1.0,
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
                     children: [
-                      const Text('골프장'),
-                      Container(
-                        width: 100.0,
-                        margin: const EdgeInsets.only(left: 10.0),
-                        color: Colors.grey[200],
-                        child: TextField(
-                          controller: TextEditingController(text: '드비치'),
-                          enabled: false,
-                          showCursor: false,
-                          textAlign: TextAlign.center,
+                      Padding(
+                        padding: const EdgeInsets.all(30.0),
+                        child: currentPage,
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 16.0),
+                          child: currentPage is ReservationCalendarForm2
+                              ? ReservationBtn(
+                            btnText: '다음',
+                            onPressed: () {
+                              switchPage(
+                                  ReservationPersonnelAndTimesetForm());
+                            },
+                            backgroundColor: Colors.black,
+                          )
+                              : ReservationBtn(
+                            btnText: '이전',
+                            onPressed: () {
+                              switchPage(ReservationCalendarForm2());
+                            },
+                            backgroundColor: Colors.black,
+                          ),
                         ),
                       ),
                     ],
                   ),
                 ),
-                const TabBar(
-                  indicatorColor: Colors.black,
-                  labelColor: Colors.black,
-                  unselectedLabelColor: Colors.grey,
-                  tabs: [
-                    Tab(text: '스케줄 예약',),
-                    Tab(text: '스케줄 조회/취소',),
-                    Tab(text: '예약 결과',),
-                  ],
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ),
-        body: PageView.builder(
-          controller: _pageController,
-          itemCount: 3,
-          onPageChanged: onPageChanged,
-          itemBuilder: (context, index) {
-            return Column(
-              children: [
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 30, top: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: List<Widget>.generate(2, (index) {
-                        return Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 4.0),
-                          width: 15.0,
-                          height: 15.0,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: currentPage == 0 ? (index == 0 ? Colors.black : Colors.white) : Colors.black,
-                            border: Border.all(
-                              color: Colors.black,
-                              width: 1.0,
-                            )
-                          ),
-                        );
-                      }),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(30.0),
-                          child: index == 0 ? ReservationCalendarForm2() : ReservationPersonnelAndTimesetForm(),
-                        ),
-                        Align(
-                          alignment: Alignment.bottomCenter, // 아래쪽 정렬
-                          child: Padding(
-                            padding: const EdgeInsets.only(bottom: 16.0), // 아래 여백 추가
-                            child: index == 0
-                                ? ReservationBtn(
-                              btnText: '다음',
-                              onPressed: () {
-                                setState(() {
-                                  _pageController.animateToPage(1, duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
-                                });
-                              },
-                              backgroundColor: Colors.black,
-                            )
-                                : ReservationBtn(
-                              btnText: '이전',
-                              onPressed: () {
-                                setState(() {
-                                  _pageController.animateToPage(0, duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
-                                });
-                              },
-                              backgroundColor: Colors.black,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-
-              ],
-            );
-          },
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.mail), label: '임시',),
-            BottomNavigationBarItem(icon: Icon(Icons.logout), label: '로그아웃',),
-          ],
-          onTap: (index) {
-            if (index == 0) {
-              // 로그아웃 처리 코드를 여기에 작성하세요.
-            }
-          },
-        ),
+          // Content for the '스케줄 조회/취소' tab (Page 2)
+          Column(
+            children: [
+              // Add your content here for the second page of '스케줄 조회/취소' tab
+            ],
+          ),
+          // Content for the '예약 결과' tab (Page 3)
+          Column(
+            children: [
+              // Add your content here for the '예약 결과' tab
+            ],
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.mail), label: '임시'),
+          BottomNavigationBarItem(icon: Icon(Icons.logout), label: '로그아웃'),
+        ],
+        onTap: (index) {
+          if (index == 0) {
+            // 로그아웃 처리 코드를 여기에 작성하세요.
+          }
+        },
       ),
     );
   }
